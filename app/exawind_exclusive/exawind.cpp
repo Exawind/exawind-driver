5e5e2154edc5ce0779ef2af141185addfa756091
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2021-07-15 11:24:57 -0600

    Enable options for user to specify rank distribution

--- app/exawind_exclusive/exawind.cpp
+++ app/exawind_exclusive/exawind.cpp
@@ -1,51 +1,91 @@
 #include "AMRWind.h"
 #include "NaluWind.h"
 #include "OversetSimulation.h"
+#include "MPIUtilities.h"
 #include "mpi.h"
 #include <filesystem>
 
 #include "yaml-cpp/yaml.h"
 #include "tioga.h"
 
+static std::string usage(std::string name)
+{
+    return "usage: " + name +
+           " [--awind NPROCS] [--nwind NPROCS] input_file\n" +
+           "\t-h,--help\t\tShow this help message\n" +
+           "\t--awind NPROCS\t\tNumber of ranks for AMR-Wind (default = all "
+           "ranks)\n" +
+           "\t--nwind NPROCS\t\tNumber of ranks for Nalu-Wind (default = all "
+           "ranks)\n";
+}
+
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        throw std::runtime_error("Usage: nalu_amr <inpfile>");
-    }
-
     MPI_Init(&argc, &argv);
     int psize, prank;
     MPI_Comm_size(MPI_COMM_WORLD, &psize);
     MPI_Comm_rank(MPI_COMM_WORLD, &prank);
 
-    // Temporarily only testing 2 MPI ranks
-    if (psize != 2) {
-        printf("This application is meant to be run with 2 processes.\n");
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    if ((argc != 2) && (argc != 4) && (argc != 6)) {
+        throw std::runtime_error(usage(argv[0]));
     }
 
-    const int num_nalu_ranks = 1;
-    const int num_amr_ranks = psize - num_nalu_ranks;
+    int num_nwind_ranks = psize;
+    int num_awind_ranks = psize;
+    std::string inpfile = "";
 
-    int amr_range[1][3] = {0, num_amr_ranks - 1, 1};
-    int nalu_range[1][3] = {num_amr_ranks, psize - 1, 1};
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            if (prank == 0) std::cout << usage(argv[0]);
+            return 0;
+        } else if (arg == "--awind") {
+            if (i + 1 < argc) {
+                std::string opt = argv[++i];
+                num_awind_ranks = std::stoi(opt);
+                if (num_awind_ranks > psize) {
+                    throw std::runtime_error(
+                        "--awind option requesting more ranks than available.");
+                }
+            } else {
+                throw std::runtime_error(
+                    "--awind option requires one argument.");
+            }
+        } else if (arg == "--nwind") {
+            if (i + 1 < argc) {
+                std::string opt = argv[++i];
+                num_nwind_ranks = std::stoi(opt);
+                if (num_nwind_ranks > psize) {
+                    throw std::runtime_error(
+                        "--nwind option requesting more ranks than available.");
+                }
+            } else {
+                throw std::runtime_error(
+                    "--nwind option requires one argument.");
+            }
+        } else {
+            inpfile = argv[i];
+        }
+    }
 
-    MPI_Group world_group, nalu_group, amr_group;
-    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-    MPI_Group_range_incl(world_group, 1, amr_range, &amr_group);
-    MPI_Group_range_incl(world_group, 1, nalu_range, &nalu_group);
+    if (num_awind_ranks + num_nwind_ranks < psize) {
+        if (prank == 0)
+            std::cout << "Warning: using fewer ranks than available ranks: MPI "
+                         "size = " +
+                             std::to_string(psize) + "; Num ranks used = " +
+                             std::to_string(num_awind_ranks + num_nwind_ranks)
+                      << std::endl;
+    }
 
-    MPI_Comm amr_comm, nalu_comm;
-    MPI_Comm_create(MPI_COMM_WORLD, amr_group, &amr_comm);
-    MPI_Comm_create(MPI_COMM_WORLD, nalu_group, &nalu_comm);
+    MPI_Comm amr_comm =
+        exawind::create_subcomm(MPI_COMM_WORLD, num_awind_ranks, 0);
+    MPI_Comm nalu_comm = exawind::create_subcomm(
+        MPI_COMM_WORLD, num_nwind_ranks, psize - num_nwind_ranks);
 
-    const std::string inpfile(argv[1]);
     const YAML::Node doc(YAML::LoadFile(inpfile));
     const YAML::Node node = doc["exawind"];
-
     const std::string amr_inp = node["amr_wind_inp"].as<std::string>();
     const std::string nalu_inp = node["nalu_wind_inp"].as<std::string>();
-
     std::filesystem::path fpath_amr_inp(amr_inp);
     const std::string amr_log =
         fpath_amr_inp.replace_extension(".log").string();
@@ -53,12 +93,12 @@ int main(int argc, char** argv)
 
     exawind::OversetSimulation sim(MPI_COMM_WORLD);
     sim.echo(
-        "Initializing AMR-Wind on " + std::to_string(num_amr_ranks) +
+        "Initializing AMR-Wind on " + std::to_string(num_awind_ranks) +
         " MPI ranks");
     if (amr_comm != MPI_COMM_NULL)
         exawind::AMRWind::initialize(amr_comm, amr_inp, out);
     sim.echo(
-        "Initializing Nalu-Wind on " + std::to_string(num_nalu_ranks) +
+        "Initializing Nalu-Wind on " + std::to_string(num_nwind_ranks) +
         " MPI ranks");
     if (nalu_comm != MPI_COMM_NULL) exawind::NaluWind::initialize();
 
